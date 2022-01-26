3ce0cf4d8cfbe11ff57c0a19e02f30fd4aaea3ed
חנצ'י פינקל <213548084@mby.co.il>
2022-01-26 16:23:43 +0200

    in my home

--- src/app/modules/update-customer/update-customer.component.ts
+++ src/app/modules/update-customer/update-customer.component.ts
@@ -11,20 +11,20 @@ export class UpdateCustomerComponent implements OnInit {
   
   constructor() { }
 
-    customerForm:FormGroup=new FormGroup({
-      "FirstName":new FormControl("",Validators.required),
-      "LastName":new FormControl("",Validators.required),
-      "NumOfAdults":new FormControl(""),
-      "NumOfKids":new FormControl(""),
-      "HighFloor":new FormControl(""),
-      "Porch":new FormControl(""),
-      "SeparteBeds":new FormControl(""),
-      "MultipleRooms":new FormControl(""),
-      "EmailAdress":new FormControl("",Validators.email),
-      "Address":new FormControl("",Validators.required),
-      "PhoneNumber":new FormControl("",Validators.required),
-      "Comments":new FormControl(""),
-    });
+    // customerForm:FormGroup=new FormGroup({
+    //   "FirstName":new FormControl("",Validators.required),
+    //   "LastName":new FormControl("",Validators.required),
+    //   "NumOfAdults":new FormControl(""),
+    //   "NumOfKids":new FormControl(""),
+    //   "HighFloor":new FormControl(""),
+    //   "Porch":new FormControl(""),
+    //   "SeparteBeds":new FormControl(""),
+    //   "MultipleRooms":new FormControl(""),
+    //   "EmailAdress":new FormControl("",Validators.email),
+    //   "Address":new FormControl("",Validators.required),
+    //   "PhoneNumber":new FormControl("",Validators.required),
+    //   "Comments":new FormControl(""),
+    // });
   ngOnInit(): void {
  
   }
