deb84818b610a12da880543a6b3c34213fffe2d4
chanchy <213548084@PC5-13>
2022-01-30 13:49:08 +0200

    30/01/2022 13:49

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,16 +12,24 @@ import {MatInputModule} from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import { CustomerModule } from './modules/cutomer.module';
+import { CustomerModule } from './modules/customer/customers/customer.module';
+import { RouterModule,Routes } from '@angular/router';
+import { CustomerComponent } from './modules/customer/customers/customer/customer.component';
+import { UpdateCustomerComponent } from './modules/customer/customers/update-customer/update-customer.component';
 //import { UpdateCustomerComponent } from './modules/update-customer/update-customer.component';
 
+const APP_ROUTES:Routes=[
+  {path:"admin",component:AdminComponent},
+  {path:"newCustomer",component:UpdateCustomerComponent},
+  //{path:"customer", loadChildren:()=> import("./modules/customer/customers/customer.module").then(m=>m.CustomerModule)},
 
+ 
+];
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
-
-
+    AdminComponent,
+   
   ],
   imports: [
     BrowserModule,
@@ -32,11 +40,13 @@ import { CustomerModule } from './modules/cutomer.module';
     HttpClientModule,
     BrowserAnimationsModule,
     CustomerModule,
+   RouterModule.forRoot(APP_ROUTES),
 
     ///////////////// material modules /////////////////
     MatInputModule,
     MatButtonModule,
     MatIconModule
+    
     
   ],
   providers: [],
