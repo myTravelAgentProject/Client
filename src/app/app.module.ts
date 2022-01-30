2e0d144fbb0c3a6cbbfbe40e9ee39bd4861154f0
chanchy <213548084@PC5-13>
2022-01-30 17:58:53 +0200

    change module place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,10 @@ import {MatInputModule} from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import { CustomerModule } from './modules/customer/customers/customer.module';
+import { CustomerModule } from './modules/customer/customer.module';
 import { RouterModule,Routes } from '@angular/router';
-import { CustomerComponent } from './modules/customer/customers/customer/customer.component';
-import { UpdateCustomerComponent } from './modules/customer/customers/update-customer/update-customer.component';
+import { CustomerComponent } from './modules/customer/customer/customer.component';
+import { UpdateCustomerComponent } from './modules/customer/update-customer/update-customer.component';
 //import { UpdateCustomerComponent } from './modules/update-customer/update-customer.component';
 
 const APP_ROUTES:Routes=[
